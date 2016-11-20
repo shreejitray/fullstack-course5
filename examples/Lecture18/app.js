cd849783c414605882148a320f222222c914ba3f
Shreejit Chaudhury <SChaudhury@walmartlabs.com>
2016-11-20 14:10:29 +0530

    custom filter for ng-repeat

--- examples/Lecture18/app.js
+++ examples/Lecture18/app.js
@@ -6,11 +6,38 @@ var shoppingList = [
 ];
 
 angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
+.controller('ShoppingListController', ShoppingListController)
+  .filter('custom' , customFilter);
 
 ShoppingListController.$inject = ['$scope'];
 function ShoppingListController($scope) {
+  $scope.shoppingList3=[
+    {name:"shreejit",desc:"godbole"},
+    {name:"kunji",desc:"shreejit"},
+    {name:"ray",desc:"suresh"},
+    {name:"suresh",desc:"ray"},
+  ];
   $scope.shoppingList = shoppingList;
+}
+
+  function containsFilter(value) {
+    return value.indexOf(search) !== -1;
+  }
+function customFilter(){
+  return function(input, search){
+    console.log(`searched item is ${search}`);
+    input.forEach(function(item){
+      console.log(`item name ${item.name}`);
+    });
+    var newArr = input.filter(function(item){
+      return item.name.indexOf(search) !== -1;
+    });
+    console.log(`new array size ${newArr.length}`);
+    if(search == undefined){
+      return input;
+    }
+    return newArr;
+  }
 }
 
 })();
